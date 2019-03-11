58fad337f453fabda50a99b6a5086b327ae65fbf
graveja0 <john.graves@vanderbilt.edu>
2019-03-11 09:01:30 -0500

    updated PSAs to include a "realistic" PSA scenario that searches over a narrower parameter range more akin to a PSA in a published CEA

--- R/simple-params.R
+++ R/simple-params.R
@@ -130,6 +130,49 @@ params_psa <- list(
 )
 
 
+params_psa_realistic <- list(
+  # Controls for model execution
+  n          = function(x) qnorm(p = x, mean = params$n,sd =0),      # DES simulations to perform
+  resolution = function(x) qnorm(p = x, mean = params$resolution ,sd = 0),        # Diff Eq Time step for DEQ approach
+  interval   = function(x) qnorm(p = x, mean = params$interval, sd = 0),            # Markov Interval
+  horizon    = function(x) qnorm(p = x, mean = params$horizon, sd = 0),           # Time horizon of simulation
+  wtp        = function(x) qnorm(p = x, mean = params$wtp, sd = 0),       # Willingness to pay threshold
+  
+  # Gompertz model of secular death for 40yr female
+  # fit from 2012 social security data
+  shape   = function(x) qnorm(p = x, mean = params$shape, sd = 0), 
+  rate    = function(x) qnorm(p = x, mean = params$rate,sd = 0),
+  
+  # Probabilities and rates
+  p_o  = function(x) qunif(p = x, min = 0.0, max = 1),                # Probability of ordering test (overwritten by runs to 0 and 1)
+  p_bd = function(x) qbeta(p = x, shape1 = params$p_bd*100, shape2 = 100-100*params$p_bd),                       # Probability of death from B
+  p_g  = function(x) qbeta(p = x, shape1 = params$p_g*100, shape2 = 100-100*params$p_g),              # Probability of genetic variant
+  r_a_pct = function(x)  100* qbeta(p = x, shape1 = params$r_a_pct, shape2 = 100-params$r_a_pct),              # Percentage who develop condition.
+  r_a_dur = function(x) qunif(p = x, min = 1e-7, max =20),              # Condition indication rate duration (in years).
+  r_b_pct = function(x) 100* qbeta(p = x, shape1 = params$r_b_pct, shape2 = 100-params$r_b_pct),               # Adverse drug event percentage
+  r_b_dur = function(x) qunif(p = x, min = 0, max = 10),               # Adverse drug event duration (in years). 
+  rr_b = function(x) qunif(p = x, min = 0.4, max = 1),                # Reduced relative risk of B
+  
+  # Costs
+  c_a   = function(x) qpois(p = x, lambda = params$c_a),              # Cost of Event A
+  c_bs  = function(x) qpois(p = x, lambda = params$c_bs),              # Cost of Event B survival
+  c_bd  = function(x) qpois(p = x, lambda = params$c_bd),             # Cost of Event B death
+  c_tx  = function(x) qpois(p = x, lambda = params$c_tx*365)/365,                 # Cost of normal treatment
+  c_alt = function(x) qpois(p = x, lambda = params$c_alt*365)/365,                # Cost of alternate treatment
+  c_t   = function(x) qunif(p = x, min = 50,max=150),               # Cost of test
+  
+  # Disutilities
+  d_a   = function(x) qbeta(p = x, shape1 = params$d_a*100, shape2 = 100-100*params$d_a),              # Disutility of A
+  d_at  = function(x) qunif(p = x, min = 0, max = 2),                 # Duration of A in years.
+  d_b   = function(x) qbeta(p = x, shape1 = params$d_b*100, shape2 = 100-100*params$d_b),              # Disutility of B
+  
+  # Discounting
+  disc  = function(x) qnorm(p = x, mean = 0.03, sd = 0)               # Annual Discount Rate
+)
+
+
+
+
 params_psa_test <- list(
   # Controls for model execution
   n          = function(x) qnorm(p = x, mean = params$n,sd =0),      # DES simulations to perform
