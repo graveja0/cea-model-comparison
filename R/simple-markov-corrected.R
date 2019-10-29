98790c4aa5feb0046521b418e102ce92d3f657a3
graveja0 <john.graves@vanderbilt.edu>
2019-10-29 16:35:09 -0500

    trying to put some code in parallel

--- R/simple-markov-corrected.R
+++ R/simple-markov-corrected.R
@@ -42,8 +42,8 @@ markov_corr_tp <- function(params, v.n, t)
     tr["TUN", "PTUN"]  <- tr["A", "D"]
     tr["TUN", "TUN"]   <- -sum(tr["A", c("BS", "BD", "D")]) # semi-Markovian
     
-    x <- as.matrix(expm(tr))
-
+    #x <- as.matrix(expm(tr))
+    x <- as.matrix(expm((1/interval) * tr))
     # Adding skip overs back to probabilities for the accumulators
     # Attempted modification for accurate discounting integration
     x["A", "CUM_BS"]    <- x["A", "BSD"]+x["A", "BS"]
