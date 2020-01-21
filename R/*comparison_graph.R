f5cfe63178dd72ddd9dc47bc83356d799bdba8fb
Zilu Zhou <zilu.zhou@vumc.org>
2020-01-21 15:23:34 -0600

    recent graphic code

--- R/*comparison_graph.R
+++ R/*comparison_graph.R
@@ -75,6 +75,7 @@ micro$logN <- factor(round(log(micro$N)))
 
 ll <- lines %>% mutate(timestep=model)
 
+tiff("Micro Simulation Convergence.tiff",res=300,units="in",width=6.5, height=5)
 # Micro Simulation Convergence
 ggplot(micro, aes(x=logN,y=NMB,color=timestep,fill=timestep),alpha=0.7) +
     geom_boxplot(coef=1e30) +
@@ -86,32 +87,38 @@ ggplot(micro, aes(x=logN,y=NMB,color=timestep,fill=timestep),alpha=0.7) +
     scale_color_manual(name="",values = c("black","darkgreen","green")) + 
     scale_size_manual(name="",values = c(0.5,0.5,0.8)) +
     xlab("Number of Simulated Patients\n(log)") + 
-    theme_bw()
-
+    theme_bw() +
+    theme(panel.grid.minor.y=element_blank())
+dev.off()
 
 #### runtime ####
 load("~/Box Sync/microsim_runs/run_time_1e7/run_t_deq.rda")
 load("~/Box Sync/microsim_runs/run_time_1e7/run_t_des.rda")
 load("~/Box Sync/microsim_runs/run_time_1e7/run_t_mark_y.rda")
+load("~/Box Sync/microsim_runs/run_time_1e7/run_t_mark_m.rda")
 load("~/Box Sync/microsim_runs/run_time_1e7/run_t_mark_d.rda")
 load("~/Box Sync/microsim_runs/run_time_1e7/run_t_mark_corr_y.rda")
+load("~/Box Sync/microsim_runs/run_time_1e7/run_t_mark_corr_m.rda")
 load("~/Box Sync/microsim_runs/run_time_1e7/run_t_micro_y.rda")
 load("~/Box Sync/microsim_runs/run_time_1e7/run_t_micro_corr_yr_nov.rda")
 
 rt <- b_deq %>% mutate(model="DEQ") %>% 
     bind_rows(b_des %>% mutate(model="DES")) %>% 
     bind_rows(b_mark_y %>% mutate(model="Markov Yearly")) %>% 
+    bind_rows(b_mark_m %>% mutate(model="Markov Monthly")) %>% 
     bind_rows(b_mark_d %>% mutate(model="Markov Daily")) %>% 
-    bind_rows(b_mark_corr_y %>% mutate(model="Markov Yearly Corrected")) %>% 
+    bind_rows(b_mark_corr_y %>% mutate(model="Markov Yearly Corrected")) %>%
+    bind_rows(b_mark_corr_m %>% mutate(model="Markov Yearly Corrected")) %>%
     bind_rows(b_micro_y %>% mutate(model="Microsim Yearly")) %>% 
     bind_rows(b_micro_corr_y %>% mutate(model="Microsim Yearly Corrected")) %>% 
     mutate(times=time/10^9)
     
-
+tiff("Runtime Comparison.tiff",res=300,units="in",width=6, height=5)
 ggplot(rt) +
     geom_boxplot(aes(x=model,y=times),outlier.shape = 1,outlier.alpha = 0.5) +
     scale_y_continuous(breaks = 10^seq(-3,4,0.5), labels = seq(-3,4,0.5), trans="log10") + ylab("log10(seconds)") + xlab("Method") +
-    coord_flip() + theme_bw()
-
+    coord_flip() + theme_bw() +
+    theme(panel.grid.major.y=element_blank())
+dev.off()
 
 
